77e3a16efa08b6e9392ea76731037a5ea290e3b8
baconbits <baconater1482@gmail.com>
2021-11-16 22:58:30 +0000

    reducing resolution to reduce cpu which gives less delay

--- production/run.sh
+++ production/run.sh
@@ -5,7 +5,8 @@ echo "running websocket"
 node jsmpeg/websocket-relay.js supersecret 8081 8082 &
 echo "done."
 echo "running ffmpeg"
-ffmpeg -f v4l2 -framerate 30 -video_size 480x360 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 480x360 -b:v 1000k -bf 0 http://10.0.2.100:8081/supersecret &
+#ffmpeg -f v4l2 -framerate 30 -video_size 480x360 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 480x360 -b:v 1000k -bf 0 http://10.0.2.100:8081/supersecret &
+ffmpeg -f v4l2 -framerate 30 -video_size 320x240 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 320x240 -b:v 1000k -bf 0 http://10.0.2.100:8081/supersecret &
 echo "done."
 echo "running webserver"
 cd production/
